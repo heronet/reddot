ae5ad23525a6c16aacd01de2225532bf44ea10d9
heronet <sirat4757@gmail.com>
2022-12-17 23:46:26 +0600

    reverted to 15

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://guarded-caverns-50373.herokuapp.com/api/",
-  socketUrl: "https://guarded-caverns-50373.herokuapp.com/"
+  apiUrl: 'https://reedit-t5in5dp7va-uc.a.run.app/api/',
+  socketUrl: 'https://reedit-t5in5dp7va-uc.a.run.app/',
 };
